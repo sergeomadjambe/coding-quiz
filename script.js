3a87a939ffda2e6a1d4a41ba9d82b5109323bacc
Serge Omadjambe <sergeomadjambe@gmail.com>
2022-12-13 20:48:58 -0500

    updated code

---
+++ script.js
@@ -0,0 +1,5 @@
+var startBtn = document.querySelector(".start-button")
+var instruction = document.queryselector(".instruction-container")
+var question = document.querySelector(".question-container")
+var score = document.querySelector(".score-container")
+
